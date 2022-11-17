99104b9800725d590399f00576378458f5994582
ahuizenga <al.huizenga@gmail.com>
2022-11-17 07:11:45 -0500

    Removed profanity from street name for negative test

--- ls_hamilton_property_test.py
+++ ls_hamilton_property_test.py
@@ -5,7 +5,7 @@ import json
 # my_address = {'street_number': '73', 'street_name': 'Tisdale', 'street_type_short': 'ST', 'street_direction_short': 'S', 'city': 'Hamilton'}
 # my_address = {'street_number': '194', 'street_name': 'King', 'street_type_short': 'ST', 'street_direction_short': 'W', 'city': 'Hamilton'}
 my_address = {'street_number': '17', 'street_name': 'Discovery', 'street_type_short': 'DR', 'city': 'Hamilton'}
-# my_address = {'street_number': '73', 'street_name': 'Fucknuts', 'street_type_short': 'DR', 'city': 'Shittsville'}
+# my_address = {'street_number': '123', 'street_name': 'Baloney', 'street_type_short': 'DR', 'city': 'Nowheresville'}
 
 
 my_prop = ls_hamilton_property_class.ls_hamilton_property(address=my_address)
